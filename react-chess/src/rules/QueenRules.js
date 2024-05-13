46789e195c8e08e3421613c966e9d68c56e70df4
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-05-12 21:03:19 -0500

    feat(rules): add QueenRules.js file
    - Export getQueenMoves function

---
+++ react-chess/src/rules/QueenRules.js
@@ -0,0 +1,66 @@
+import * as GF from "./GeneralFunctions";
+
+export const getQueenMoves = (pieceAndColor, square, isCheck, checkSquares) => {
+    const variations = [
+        [-1, 0],
+        [-1, 1],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [1, -1],
+        [0, -1],
+        [-1, -1],
+    ];
+    let movements = [];
+    for (const variation of variations) {
+        for (let i = 1; i < 8; i++) {
+            const validateSquare = GF.getAnotherSquare(i * variation[0], i * variation[1], square);
+            if (!validateSquare) {
+                break;
+            }
+            if (!GF.validateMove(validateSquare, pieceAndColor[1], GF.ACTIONS.F)) {
+                break;
+            }
+            if (
+                (variation[0] === 0 && variation[1] === 1) ||
+                (variation[0] === 0 && variation[1] === -1)
+            ) {
+                if (GF.validateHorizontalPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalRightPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalLeftPin(pieceAndColor, square)) break;
+            } else if (
+                (variation[0] === 1 && variation[1] === 0) ||
+                (variation[0] === -1 && variation[1] === 0)
+            ) {
+                if (GF.validateVerticalPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalRightPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalLeftPin(pieceAndColor, square)) break;
+            } else if (
+                (variation[0] === -1 && variation[1] === -1) ||
+                (variation[0] === 1 && variation[1] === 1)
+            ) {
+                if (GF.validateHorizontalPin(pieceAndColor, square)) break;
+                if (GF.validateVerticalPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalLeftPin(pieceAndColor, square)) break;
+            } else {
+                if (GF.validateHorizontalPin(pieceAndColor, square)) break;
+                if (GF.validateVerticalPin(pieceAndColor, square)) break;
+                if (GF.validateDiagonalRightPin(pieceAndColor, square)) break;
+            }
+            if (isCheck) {
+                for (const square of checkSquares[0]) {
+                    if (validateSquare === square) {
+                        movements.push(validateSquare);
+                    }
+                }
+            } else {
+                movements.push(validateSquare);
+            }
+            const childrens = document.getElementById(validateSquare).children;
+            if (childrens.length > 0) {
+                break;
+            }
+        }
+    }
+    return movements;
+};
